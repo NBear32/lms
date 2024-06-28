e9af500116d10516fd965f9d013c66dd0eaf1bb6
NBear32 <hansel32153@gmail.com>
2024-06-28 09:22:44 +0900

    Initial commit

---
+++ src/main/java/com/dw/lms/service/Lms_noticesService.java
@@ -0,0 +1,33 @@
+package com.dw.lms.service;
+
+import com.dw.lms.model.Lms_notices;
+import com.dw.lms.repository.Lms_noticesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Lms_noticesService {
+    @Autowired
+    private Lms_noticesRepository repository;
+
+    public Page<Lms_notices> getAllNotices(int page, int size) {
+        return repository.findAll(PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "lmsNoticesWritingDate")));
+    }
+
+    public Lms_notices getNoticeById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Lms_notices saveNotice(Lms_notices notice) {
+        return repository.save(notice);
+    }
+
+    public void deleteNotice(Long id) {
+        repository.deleteById(id);
+    }
+}
